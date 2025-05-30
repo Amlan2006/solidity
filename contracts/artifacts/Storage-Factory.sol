fd4dc8d44dd2b4206a567f23ab0cda25a27115c4
Amlan2006 <amlanroy2020@gmail.com>
2025-05-30 21:09:17 +0530

    created a contract to deploy other contracts

--- contracts/artifacts/Storage-Factory.sol
+++ contracts/artifacts/Storage-Factory.sol
@@ -4,38 +4,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18; //solidity version
 
-contract SimpleStorage {
-    //basic types: boolean, uint, int,address,bytes
-    uint public favoriteNumber;
-    uint[] listOfFavoriteNumbers;
-    mapping (string =>uint256) public nameToFavouriteNumber;
-    struct Person {
-        uint hisfavoriteNumber;
-        string name;
-    }
-    Person[] public listofPerson;
-    // Person public  pat = Person({hisfavoriteNumber:0, name:"No name"});
-    
-    function store(uint favorite) public {
-        favoriteNumber = favorite;
-        // favoriteNumber = favoriteNumber +1;
-    }
-    function addPerson(string memory _name , uint256 _favoriteNumber) public {
-listofPerson.push(Person(_favoriteNumber , _name));
-nameToFavouriteNumber[_name]=_favoriteNumber;
-    }
-    function retrieve() public view returns(uint)  {
-        return favoriteNumber;
-    }       //view,pure function disallows updating state    
-
-    
-}
+import {SimpleStorage} from "contracts/SimpleStorage.sol";
 
 
-
+ 
 contract StorageFactory{
-    SimpleStorage public simplestorage;
+    SimpleStorage[] public listofsimplestorageContracts;
     function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listofsimplestorageContracts.push(newSimpleStorageContract);
+    
 
     }
 }
